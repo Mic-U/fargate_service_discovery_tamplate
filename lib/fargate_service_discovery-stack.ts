446c3fe0ab0982c7f5d387387f36abe22bd2e3c2
Mic-U <yuho.miki@gmail.com>
2019-09-26 13:17:29 +0900

    Add ALB resource.

--- lib/fargate_service_discovery-stack.ts
+++ lib/fargate_service_discovery-stack.ts
@@ -1,6 +1,7 @@
 import cdk = require('@aws-cdk/core');
 import {VPCResources} from './vpc_resources';
 import {FargateResources} from './fargate_resources';
+import {ALBResources} from './alb_resources';
 
 export class FargateServiceDiscoveryStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,6 +9,11 @@ export class FargateServiceDiscoveryStack extends cdk.Stack {
     const vpcResources = new VPCResources(this, 'VPCResources');
     const fargateResources = new FargateResources(this, 'FargateResources', {
       vpc: vpcResources.vpc
+    });
+
+    const albResouces = new ALBResources(this, 'ALBResources', {
+      vpc: vpcResources.vpc,
+      fargateService: fargateResources.fargateService
     });
   }
 }
